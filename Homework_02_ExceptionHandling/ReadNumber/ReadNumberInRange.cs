7c1f3e44a9a2c8bef9aaa776fbcfb3c90682d68f
Venelin <ipconfigv@gmail.com>
2014-09-26 12:24:19 +0300

    ExceptionHandling

---
+++ Homework_02_ExceptionHandling/ReadNumber/ReadNumberInRange.cs
@@ -0,0 +1,121 @@
+﻿namespace ReadNumber
+{
+    using System;
+    using System.Collections.Generic;
+
+    class ReadNumberInRange
+    {
+        public static string ReadNumber(int start, int end)
+        {
+            List<int> list = new List<int>();
+            int num;
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    num = Int32.Parse(Console.ReadLine());
+                    if (num <= start || num >= end)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    start = num;
+                    list.Add(num);
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Not a number !");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Not in the range ({0}:{1})!", start, end);
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("The number is very big for an int value !");
+                }
+                catch (ArgumentNullException)
+                {
+                    Console.WriteLine("You have entered empty space!");
+                }
+            }
+
+            int[] array = list.ToArray();
+            string result = string.Join(" ", array);
+
+            return result.ToString();
+        }
+
+        private static int MyTryParseEnd(int end)
+        {
+            try
+            {
+                if (end > 100)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Have entered invalid number for the End of the range!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number !");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number is very big for an int value !");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You have entered empty space!");
+            }
+            return end;
+        }
+
+        private static int MyTryParse(int start)
+        {
+            try
+            {
+                if (start < 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Have entered invalid number for start of the range!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number !");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("The number is very big for an int value !");
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("You have entered empty space!");
+            }
+            return start;
+        }
+
+        static void Main()
+        {
+            int start = 0;
+            int end = 0;
+
+            Console.WriteLine("Enter start number for the range: ");
+            start = MyTryParse(start = Int32.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Enter the End number for the range: ");
+            end = MyTryParseEnd(end = Int32.Parse(Console.ReadLine()));
+
+            int number;
+            string sequence = ReadNumber(start, end);
+
+            Console.WriteLine("Correctly inserted sequence is: {0}", sequence);
+        }
+    }
+}
---
+++ Homework_02_ExceptionHandling/SquareRoot/SqrtExeption.cs
@@ -0,0 +1,45 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    class SqrtExeption
+    {
+        public static double Sqrt(double num)
+        {
+            if (num < 0)
+            {
+                throw new System.ArgumentOutOfRangeException();
+            }
+
+            return Math.Sqrt(num);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter some int number: ");
+
+            try
+            {
+                int number = Int32.Parse(Console.ReadLine());
+
+                try
+                {
+                    double result = Sqrt(number);
+                    Console.WriteLine("The square of {0} is {1} !", number, result);
+                }
+                catch (ArgumentException nan)
+                {
+                    Console.WriteLine("Invalid number(negative)!");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye!\n");
+            }
+        }
+    }
+}
